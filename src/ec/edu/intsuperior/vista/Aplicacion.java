0e5d597c9792645c4cf01904d89d6ab62dd5753b
Anthony Sanchez <Anthony Sanchez@DESKTOP-191A81L>
2023-05-02 19:30:45 -0500

    todos mis ejercicios ing

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+  import java.util.Scanner;
+
+/**
+ *
+ * @author Vaio
+ */
+public class Aplicacion {
+    
+     static Scanner leer = new Scanner (System.in);
+    public static void main(String[] args) {
+        
+    }
+    public static void metodol(){
+        int i=1;
+        while (i<=5){
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+    public static void metodo2(){
+        for (int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+        
+    }
+    public static void metodo3(){
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=5);
+    }
+    public static void adivinaNumero (){
+    int n=(int)(Math.random()*10);
+        int adivina;
+        do{
+        System.out.println(n+"ingresa un numero para ver si adivinas");
+         adivina=leer.nextInt();
+        if (adivina==n) {
+            System.out.println("Felicidades ganaste");
+        } else {
+            System.out.println("Intenta de nuevo");
+        }
+        }while(adivina!=n);
+    }
+    }
